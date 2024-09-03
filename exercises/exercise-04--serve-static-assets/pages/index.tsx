78786db7b43c53d64aade4ccf3593b8544ab8788
Danny Tunney <dtun@me.com>
2024-09-03 08:04:49 -0700

    Serve Static Assets in a Next.js Project

--- exercises/exercise-04--serve-static-assets/pages/index.tsx
+++ exercises/exercise-04--serve-static-assets/pages/index.tsx
@@ -4,6 +4,7 @@ const Home = () => {
       <h2>Hello 👋</h2>
       <p>My name is Lazar Nikolov</p>
       {/* ✍️ add the "lazar.png" image here */}
+      <img alt="lazar, the man" src="/lazar.png" />
     </div>
   )
 }
