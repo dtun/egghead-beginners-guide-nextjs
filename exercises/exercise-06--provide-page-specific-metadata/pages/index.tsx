126b3429e797d8530309608a617b1338a12bc260
Danny Tunney <dtun@me.com>
2024-09-03 08:28:41 -0700

    Provide page-specific Metadata Using the Head Component

--- exercises/exercise-06--provide-page-specific-metadata/pages/index.tsx
+++ exercises/exercise-06--provide-page-specific-metadata/pages/index.tsx
@@ -1,10 +1,18 @@
 // ✍️ import Head from 'next/head'
 import { ChakraProvider, Heading } from '@chakra-ui/react'
+import Head from 'next/head'
 
 const Home = () => {
   return (
     <ChakraProvider>
       {/* ✍️ add title and description using the Head component */}
+      <Head>
+        <title>Homepage</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Head>
+        <meta name="og:title" content="My NextJS course" key="title" />
+      </Head>
       <Heading w="full" textAlign="center">
         This is our homepage content!
       </Heading>
