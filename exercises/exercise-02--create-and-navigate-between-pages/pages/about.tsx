5d17223037aedc41570cc3060fb67b1ec3da8088
Danny Tunney <dtun@me.com>
2024-09-03 07:45:46 -0700

    Create New Pages in Next.js and Navigate Them Using Link

---
+++ exercises/exercise-02--create-and-navigate-between-pages/pages/about.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const About = () => {
+  return (
+    <>
+      <h1>This is the About page</h1>
+      <Link href="/">Back to Home</Link>
+    </>
+  )
+}
+
+export default About
