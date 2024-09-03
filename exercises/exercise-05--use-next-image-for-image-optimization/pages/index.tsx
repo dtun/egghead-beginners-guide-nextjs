d843664e427ba74aeb06c2fa12184af1fb72b46a
Danny Tunney <dtun@me.com>
2024-09-03 08:12:59 -0700

    Serve Optimized Images Using the Next.js Image Component

--- exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
+++ exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
@@ -1,11 +1,19 @@
 // ✍️ import the Image component from 'next/image'
+import Image from 'next/image'
 
 // ✍️ import "lazar.png" statically
+import lazar from '../lazar.png'
+
+const TREE_URL =
+  'https://images.immediate.co.uk/production/volatile/sites/10/2023/06/2048x1365-Oak-trees-SEO-GettyImages-90590330-b6bfe8b.jpg?quality=90&resize=940,627'
 
 const Home = () => {
-  return {
-    /* ✍️ add the lazar image here */
-  }
+  return (
+    <>
+      <Image alt="lazar, the man" src={lazar} />
+      <Image alt="a tree" src={TREE_URL} />
+    </>
+  )
 }
 
 export default Home
